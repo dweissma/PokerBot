408f6542bcd4bd037c3a682c8ae0d355289b0886
chongu <chongu@iu.edu>
2019-11-12 22:37:05 -0500

    playeranduser

--- Player.py
+++ Player.py
@@ -5,9 +5,9 @@ File for basic player class
 from Game import Game
 
 class Player(Game):
-    def __init__(self):
-        self.hand = None #The player's hand
-        self.money = None #How much money/chips the player has
+    def __init__(self, money, hand):
+        self.hand = hand #The player's hand
+        self.money = money #How much money/chips the player has
         self.playing = True # if fold, turn it to false
         raise NotImplementedError()
 
@@ -20,25 +20,17 @@ class Player(Game):
         The second element being the amount of the bet
         if applicable
         return -1 if not
-
         """
-        # to ensure a valid input
-        while True:
-            if action == 'r':
-                # In case the player gives an invalid amount
-                while True:
-                    # should add some rules
-                    # amount = int(input("How much do you want to raise? "))
-                    if amount >= self.min:
-                        self.money -= amount
-                        self.pot += amount
-                        break
-            elif action == 'f':
-                self.playing = False
-                break
-            elif action == 'c':
-                print("Your hand: " + self.hand)
-                break
+
+        if action == 'r':
+            self.money -= amount
+            self.pot += amount
+        elif action == 'c':
+            print("Hand: "+self.hand)
+        else:
+            self.playing = False
+
+
 
         raise NotImplementedError()
 
--- User.py
+++ User.py
@@ -1,12 +1,43 @@
 """
 A class for users to play games of holdem
 """
+import os
 
 from Player import Player
 
 class User(Player):
+
     def play(self):
-        action = input("It's your turn: ")
+        # get a valid action
+        while True:
+            try:
+                action = input("It's your turn: ")
+                if action in ['r', 'c', 'f', 'q']:
+                    break
+                else:
+                    raise ValueError
+            except ValueError:
+                print("Invalid input! Try again.")
+
+        if action == 'r':
+            rasieAmount = int(input("How much do you want to raise? "))
+            # get a valid raise
+            while True:
+                try:
+                    if self.money >= rasieAmount >= self.min:
+                        self.bet('r', rasieAmount)
+                    else:
+                        raise ValueError
+                except ValueError:
+                    print("Invalid input! Try again.")
+        elif action == 'c':
+            print("Your hand: ", self.hand)
+            self.bet('c', -1)
+        elif action == 'f':
+            self.bet('f', -1)
+        else:
+            # if we get a 'q', quit the game
+            os.quit()
 
     raise NotImplementedError()
 
