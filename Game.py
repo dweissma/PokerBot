71f214531deda13edd82e1059e4293834a00bd09
Andrew Weissmann <37910228+dweissma@users.noreply.github.com>
2019-11-12 21:31:49 -0500

    Added a deck with card encodings to Game

--- Game.py
+++ Game.py
@@ -2,6 +2,9 @@
 Runs a game of texas holdem
 """
 
+from itertools import product
+from random import shuffle
+
 class Game(object):
     STAGES = {
         'begin':'B',
@@ -9,6 +12,13 @@ class Game(object):
         'turn':'T',
         'river':'R',
     }    
+    RANKS = ['2', '3',' 4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    SUITS = ['H', 'D', 'S', 'C'] # Heart-H, Diamond-D, Spade-S, Club-C
+
+    #A card is represented as a suit rank tuple given as (suit, rank)
+    #For example a Jack of hearts would be ('H', 'J')
+
+    DECK = list(product(RANKS, SUITS))
 
     def __init__(self, players, money):
         self.players = []
@@ -17,6 +27,8 @@ class Game(object):
         self.stage = Game.STAGES['begin']
         self.pot = 0
         self.min = 0 # every time you bet, should greater than this number
+        self.deck = self.DECK[:]
+        shuffle(self.deck)
         raise NotImplementedError()
 
     def play_hand(self):
